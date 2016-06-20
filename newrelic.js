06f23b447459baee86f24ab89f1b3ce62778c81b
Max Beatty <max@beatty.me>
2016-06-20 14:52:32 -0700

    add newrelic and rollbar monitoring

---
+++ newrelic.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+    app_name: ['Pass Plum'], // eslint-disable-line camelcase
+    license_key: process.env.NEW_RELIC_LICENSE_KEY, // eslint-disable-line camelcase
+    logging: {
+        /**
+         * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+         * issues with the agent, 'info' and higher will impose the least overhead on
+         * production applications.
+         */
+        level: 'info'
+    }
+};
