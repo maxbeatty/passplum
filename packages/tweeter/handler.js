532da4733d5b31015f020bee896c2cbf3ca27d17
Max Beatty <max@beatty.me>
2017-09-05 11:01:10 -0700

    stub tests for tweeter

--- packages/tweeter/handler.js
+++ packages/tweeter/handler.js
@@ -1,4 +1,4 @@
-const { get, post } = require("request");
+const {get, post} = require('request');
 
 const {
   TWITTER_CONSUMER_KEY,
@@ -10,44 +10,43 @@ const {
 const RE = /<code\s.+?>(.+?)<\/code>/g;
 
 module.exports.tweeter = (event, context, callback) => {
-  get("https://passplum.com", function(err, res, body) {
-    console.log("response status: " + res.statusCode);
-    // console.log("response body", body);
+	get('https://passplum.com', (err, res, body) => {
+		console.log('response status: ' + res.statusCode);
 
-    if (err) {
-      console.log("problem with request", err);
-      return callback(err);
-    }
+		if (err) {
+			console.log('problem with request', err);
+			return callback(err);
+		}
 
-    const matches = RE.exec(body);
+		const matches = RE.exec(body);
 
-    if (matches && matches.length > 1) {
-      console.log("found passphrase", matches[1]);
+		if (matches && matches.length > 1) {
+			console.log('found passphrase', matches[1]);
 
-      post(
-        {
-          url: "https://api.twitter.com/1.1/statuses/update.json",
-          oauth: {
-            consumer_key: TWITTER_CONSUMER_KEY,
-            consumer_secret: TWITTER_CONSUMER_SECRET,
-            token: TWITTER_ACCESS_TOKEN,
-            token_secret: TWITTER_ACCESS_SECRET
-          },
-          qs: {
-            status: "Here is a great password: " + matches[1]
-          }
-        },
-        function(err) {
-          if (err) {
-            console.log(err);
-            callback(err);
-          } else {
-            callback();
-          }
-        }
+			post(
+				{
+					url: 'https://api.twitter.com/1.1/statuses/update.json',
+					oauth: {
+						consumer_key: TWITTER_CONSUMER_KEY, // eslint-disable-line camelcase
+						consumer_secret: TWITTER_CONSUMER_SECRET, // eslint-disable-line camelcase
+						token: TWITTER_ACCESS_TOKEN,
+						token_secret: TWITTER_ACCESS_SECRET // eslint-disable-line camelcase
+					},
+					qs: {
+						status: 'Here is a great password: ' + matches[1]
+					}
+				},
+        err => {
+	if (err) {
+		console.log(err);
+		callback(err);
+	} else {
+		callback();
+	}
+}
       );
-    } else {
-      callback(new Error("could not find passphrase"));
-    }
-  });
+		} else {
+			callback(new Error('could not find passphrase'));
+		}
+	});
 };
